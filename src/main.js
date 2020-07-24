9ea3218f16a95f5d1d0f85d7af40965e35b9eb1b
Lucy Murray <lucy.murray@bekagool.com>
2020-07-24 16:51:32 +0100

    use vue pwa plugin

--- src/main.js
+++ src/main.js
@@ -1,18 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import SW from '../serviceWorker';
-// SW;
-Vue.config.productionTip = false
+import './registerServiceWorker'
 
-// Register the service worker if available.
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/serviceWorker.js').then(function (reg) {
-      console.log('Successfully registered service worker', reg);
-      
-  }).catch(function (err) {
-      console.warn('Error whilst registering service worker', err);
-  });
-}
+Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
