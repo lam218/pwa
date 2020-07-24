5358fc8fd26b2b4af9065b9fca530fc6f5187473
Lucy Murray <lucy.murray@bekagool.com>
2020-07-24 14:55:48 +0100

    change app to do app

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav-header />
+    <todo-list list-name="My to-dos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from "./components/ToDoList";
+import NavHeader from "./components/Header";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodoList,
+    NavHeader
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
