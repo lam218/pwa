770e406146c517049471683ac1c48cfd187df096
Lucy Murray <lucy.murray@bekagool.com>
2020-07-24 11:55:32 +0100

    add PWA features

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// import SW from '../serviceWorker';
+// SW;
 Vue.config.productionTip = false
+
+// Register the service worker if available.
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/serviceWorker.js').then(function (reg) {
+      console.log('Successfully registered service worker', reg);
+
+  }).catch(function (err) {
+      console.warn('Error whilst registering service worker', err);
+  });
+}
 
 new Vue({
   render: h => h(App),
